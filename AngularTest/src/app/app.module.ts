543d09dded2cba2014eb0d336cee5c7e3754f779
Yako <jakooste@outlook.com>
2024-12-10 20:54:01 +0100

    add members tab

--- AngularTest/src/app/app.module.ts
+++ AngularTest/src/app/app.module.ts
@@ -3,38 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
 import { CoursesComponent } from './courses.component';
 import {CoursesService} from './courses.service';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
 import { WpisWydatekComponent } from './wpis-wydatek/wpis-wydatek.component';
 import { TabWydatkiComponent } from './tab-wydatki/tab-wydatki.component';
 import { RachunekMainComponent } from './rachunek-main/rachunek-main.component';
 import { RachunekHeaderComponent } from './rachunek-header/rachunek-header.component';
 import { RachunekScreenComponent } from './rachunek-screen/rachunek-screen.component';
 import {NgOptimizedImage} from "@angular/common";
+import { TabCzlonkowieComponent } from './tab-czlonkowie/tab-czlonkowie.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent,
     CoursesComponent,
-    HeaderComponent,
-    MainComponent,
-    FooterComponent,
     WpisWydatekComponent,
     TabWydatkiComponent,
     RachunekMainComponent,
     RachunekHeaderComponent,
-    RachunekScreenComponent
+    RachunekScreenComponent,
+    TabCzlonkowieComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NgOptimizedImage
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgOptimizedImage,
+    FormsModule
+  ],
   providers: [
     CoursesService,
   ],
